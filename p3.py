3180d2f53095bf9bb29d073b2c2098041e7081da
Santiago Martinez-Avial <santiago.martinez@code.berlin>
2021-01-14 05:45:01 +0100

    updated p3

--- p3.py
+++ p3.py
@@ -1,10 +1,12 @@
 num = 600851475143
 def is_prime(n):
     if n <= 1: return False
-    if n <= 3: return True
-    if (n % 2 == 0 or n % 3 == 0): return False
-    for i in range(5, n, 6):
-        if ( n % i == 0 or n % (i+2) == 0): return False
+    if n <= 3: return(True)
+    if n % 2 == 0 or n % 3 == 0: return False
+    i = 5
+    while i**2 <= n:
+        if n % i == 0 or n % (i+2) == 0: return False
+        i += 6
     return True
 i = 2
 while True:
