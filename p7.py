dc124a1160f141114653e4dfa5645d7689a6d812
Santiago Martinez-Avial <santiago.martinez@code.berlin>
2021-01-14 05:57:43 +0100

    p7

---
+++ p7.py
@@ -0,0 +1,16 @@
+def is_prime(n):
+    if n <= 1: return False
+    if n <= 3: return(True)
+    if n % 2 == 0 or n % 3 == 0: return False
+    i = 5
+    while i**2 <= n:
+        if n % i == 0 or n % (i+2) == 0: return False
+        i += 6
+    return True
+
+count = 1
+i = 1
+while count < 10001:
+    i += 2
+    count += is_prime(i)
+print(i)
