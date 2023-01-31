4803c511e3a70a66f15169891cc218ec9f75dd38
Mavial <santi.mavial@gmail.com>
2023-01-31 13:56:00 +0100

    improved is_prime()

--- p003.py
+++ p003.py
@@ -1,20 +1,31 @@
 num = 600851475143
+
+
 def is_prime(n):
-    if n <= 1: return False
-    if n <= 3: return(True)
-    if n % 2 == 0 or n % 3 == 0: return False
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
     i = 5
     while i**2 <= n:
-        if n % i == 0 or n % (i+2) == 0: return False
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
         i += 6
     return True
+
+
 i = 2
 while True:
-    if not num % i == 0: i += 1
+    if not num % i == 0:
+        i += 1
     else:
-        if is_prime(int(num/i)): break
-        else: i += 1
+        if is_prime(int(num / i)):
+            break
+        else:
+            i += 1
 
-print(int(num/i))
+print(int(num / i))
 
 # not very proud of this.
--- p007.py
+++ p007.py
@@ -1,12 +1,17 @@
 def is_prime(n):
-    if n <= 1: return False
-    if n <= 3: return(True)
-    if n % 2 == 0 or n % 3 == 0: return False
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
     i = 5
     while i**2 <= n:
-        if n % i == 0 or n % (i+2) == 0: return False
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
         i += 6
     return True
+
 
 count = 1
 i = 1
